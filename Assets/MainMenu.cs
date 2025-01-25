7b882da55b92c5f4b9beb995cd4e532ffbb69900
Voxian22 <justusnielsen@gmail.com>
2025-01-25 12:31:30 -0400

    MainMenu
    Added MainMenu UI and bubble particle effects

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
