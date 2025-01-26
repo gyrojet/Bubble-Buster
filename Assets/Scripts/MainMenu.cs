dba75c5bf0f40c884ae218b5ee37579ceda1f5a6
gyrojet <Shinnok1122@gmail.com>
2025-01-26 10:49:46 -0500

    Sunday 2
    Pushing

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Main");
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
